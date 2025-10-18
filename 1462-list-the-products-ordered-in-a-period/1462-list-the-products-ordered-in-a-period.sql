073412255dfe050c002e9b03dd3269dec42cfc2d
DP <145845508+Parth-bot-crypto26@users.noreply.github.com>
2025-10-18 18:01:42 +0530

    Time: 698 ms (53.26%), Space: 0B (100%) - LeetHub

---
+++ 1462-list-the-products-ordered-in-a-period/1462-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,13 @@
+select
+  p.product_name,
+  sum(o.unit) as unit
+from
+  products as p
+  join orders as o on p.product_id = o.product_id
+where
+  year(o.order_date) = 2020 and month(o.order_date) = 2
+group by
+  p.product_id,
+  p.product_name
+having
+  sum(o.unit) >= 100;
