41ad49abf78514beff8ab3e9e78968ddd668197a
DP <145845508+Parth-bot-crypto26@users.noreply.github.com>
2025-11-06 19:54:04 +0530

    Time: 746 ms (55.24%), Space: 0B (100%) - LeetHub

---
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -0,0 +1,17 @@
+SELECT
+    p.product_id,
+    COALESCE(
+        ROUND(
+            CAST(SUM(u.units * p.price) AS DECIMAL(10, 2)) / SUM(u.units),
+            2
+        ),
+        0
+    ) AS average_price
+FROM
+    Prices p
+LEFT JOIN
+    UnitsSold u 
+    ON p.product_id = u.product_id
+    AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY
+    p.product_id;
