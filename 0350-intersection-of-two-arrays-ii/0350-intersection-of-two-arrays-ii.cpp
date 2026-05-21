107dfeb793c139788d224c4cc300fd0e172651b7
DP <145845508+Parth-bot-crypto26@users.noreply.github.com>
2026-05-21 22:19:39 +0530

    Time: 0 ms (100%), Space: 14.8 MB (30.87%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        map<int, int> freqMap;
-        for(int num : nums1){
-            freqMap[num]++;
+        if(nums1.size() > nums2.size()){
+            return intersect(nums2, nums1);
         }
-
+        unordered_map<int, int> counts;
+        for(int num : nums1){
+            counts[num]++;
+        }
         vector<int> result;
         for(int num : nums2){
-            if(freqMap.count(num) && freqMap[num] > 0){
+            if(counts[num] > 0){
                 result.push_back(num);
-                freqMap[num]--;
+                counts[num]--;
             }
         }
         return result;
