899560abc949ff0bec6c1c7948a50d12021ee537
DP <145845508+Parth-bot-crypto26@users.noreply.github.com>
2026-07-02 22:03:26 +0530

    Time: 0 ms (100%), Space: 9.5 MB (15.63%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        int stRow = 0, stCol = 0, endRow = m-1, endCol = n-1;
+        vector<int> ans;
+        while(stRow <= endRow && stCol <= endCol){
+            for(int j = stCol; j <= endCol;  j++){
+                ans.push_back(matrix[stRow][j]);
+            }
+            for(int i = stRow+1; i <= endRow; i++){
+                ans.push_back(matrix[i][endCol]);
+            }
+            for(int j = endCol-1; j >= stCol; j--){
+                if(stRow == endRow){
+                    break;
+                }
+                ans.push_back(matrix[endRow][j]);
+            }
+            for(int i = endRow-1; i >= stRow+1; i--){
+                if(stCol == endCol){
+                    break;
+                }
+                ans.push_back(matrix[i][stCol]);
+            }
+            stRow++; endRow--; stCol++; endCol--;
+        }
+        return ans;
+    }
+};
