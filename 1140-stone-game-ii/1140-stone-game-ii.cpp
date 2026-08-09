65003e1d530a87fb3a867614fd7e6ee12fdfabe3
DP <145845508+Parth-bot-crypto26@users.noreply.github.com>
2026-08-09 08:21:34 +0530

    Time: 42 ms (20.28%), Space: 13.7 MB (55.9%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int stoneGameII(vector<int>& piles) {
+        int n = piles.size();
+        vector<int> suffixSum(n+1, 0);
+        for(int i = n-1; i >= 0; --i){
+            suffixSum[i] = suffixSum[i+1] + piles[i];
+        }
+        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        for(int i = n-1; i >= 0; --i){
+            for(int M = 1; M <= n; ++M){
+                for(int X = 1; X <= 2*M && i+X <= n; ++X){
+                    dp[i][M] = max(dp[i][M], suffixSum[i]-dp[i+X][max(M, X)]);
+                }
+            }
+        }
+        return dp[0][1];
+    }
+};
